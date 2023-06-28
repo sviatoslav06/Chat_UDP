29bffbadda8c8f1d7d4b6786af05d08c8ea3c1d3
samol <samol@DESKTOP-M1TF6E9>
2023-06-28 23:17:02 +0300

    Add project files.

---
+++ chat_client/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chat_client
+{
+    public partial class MainWindow : Window
+    {
+        UdpClient client = new();
+        private bool inChat = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessageBtnClick(object sender, RoutedEventArgs e)
+        {
+            if (!inChat)
+            {
+                MessageBox.Show("You are not a member of a chat!");
+                return;
+            }
+            SendMessage(msgTxtBox.Text);
+        }
+
+        private void JoinBtnClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<join>");
+            SendMessage($"'{nameTxtBox.Text}' joined chat...");
+            inChat = true;
+            nameTxtBox.IsEnabled = false;
+            portTxtBox.IsEnabled = false;
+            ipTxtBox.IsEnabled = false;
+            Listen();
+        }
+
+        private void LeaveBtnClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<leave>");
+            SendMessage($"'{nameTxtBox.Text}' left chat...");
+            inChat = false;
+            nameTxtBox.IsEnabled = true;
+            portTxtBox.IsEnabled = true;
+            ipTxtBox.IsEnabled = true;
+        }
+
+        private async void SendMessage(string text)
+        {
+            IPEndPoint serverIp = new IPEndPoint(IPAddress.Parse(ipTxtBox.Text), int.Parse(portTxtBox.Text));
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            await client.SendAsync(data, serverIp);
+        }
+
+        private async void Listen()
+        {
+            while (inChat)
+            {
+                var res = await client.ReceiveAsync();
+                string message = Encoding.UTF8.GetString(res.Buffer);
+                msgList.Items.Add(message);
+            }
+        }
+    }
+}
---
+++ chat_server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 3737;
+const string JOIN_CMD = "<join>";
+const string LEAVE_CMD = "<leave>";
+int count = 0;
+
+UdpClient server = new(port);
+
+//ignore dublicates
+HashSet<IPEndPoint> members = new HashSet<IPEndPoint>();
+
+while (true)
+{
+    IPEndPoint clientIp = null;
+    byte[] data = server.Receive(ref clientIp);
+    string message = Encoding.UTF8.GetString(data);
+    Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] - {message} | from {clientIp}");
+    switch (message)
+    {
+        case JOIN_CMD:
+            if(count >= 10)
+            {
+                foreach (IPEndPoint ip in members)
+                    server.Send(Encoding.UTF8.GetBytes("Wait untill someone leave chat!!!"), ip);
+            }
+            else
+            {
+                members.Add(clientIp);
+                count++;
+            }
+            break;
+        case LEAVE_CMD:
+            members.Remove(clientIp);
+            count--;
+            break;
+        default:
+            foreach (IPEndPoint ip in members)
+                server.Send(data, ip);
+            break;
+    }
+}
